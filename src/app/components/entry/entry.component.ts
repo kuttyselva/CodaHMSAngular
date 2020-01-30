b6c6b1a199726fb715337750aa14096574329867
kuttyselva <33373961+kuttyselva@users.noreply.github.com>
2020-01-31 00:57:26 +0530

    somehow

--- src/app/components/entry/entry.component.ts
+++ src/app/components/entry/entry.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { PatientService } from 'src/app/services/patient/patient.service';
 import { DoctorService } from 'src/app/services/doctor/doctor.service';
+import { User } from 'src/app/models/user.models';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-entry',
@@ -11,24 +13,35 @@ import { DoctorService } from 'src/app/services/doctor/doctor.service';
 export class EntryComponent implements OnInit {
   patientEntryForm: FormGroup;
   invalid: boolean;
-  constructor(private formBuilder: FormBuilder, private patientService: PatientService ,private doctorService: DoctorService) { }
+  user: User;
+  valid: boolean;
+  constructor(private formBuilder: FormBuilder, private patientService: PatientService,
+    private doctorService: DoctorService, private store: Store<{ user: User }>) {
 
+  }
   ngOnInit() {
     this.patientEntryForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      disease: ['', Validators.required],
-      id: ['', Validators.required],
-      phone: ['', Validators.required]
-
+      name: [''],
+      disease: [''],
+      id: [''],
+      phone: ['']
     });
+    this.store.select('user').subscribe(data => {
+      this.user = data;
+    });
+
   }
   patientEntry(patientEntryForm: FormGroup) {
-    
- }
+    this.doctorService.addEntry(this.user.id, patientEntryForm.value.id).subscribe((data) => {
+      this.valid = true;
+    }, err => {
+      this.invalid = true;
+    });
+  }
   getPatient(patientEntryForm: FormGroup) {
     this.patientService.getPatient(patientEntryForm.value.id).subscribe((data) => {
       if (data.object) {
-        this.invalid=false;
+        this.invalid = false;
         patientEntryForm.setValue({
           name: data.object.name,
           disease: data.object.disease,
@@ -36,8 +49,8 @@ export class EntryComponent implements OnInit {
           id: patientEntryForm.value.id
         });
       }
-      else{
-        this.invalid=true;
+      else {
+        this.invalid = true;
       }
     });
   }
--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -3,31 +3,30 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent
-} from "@angular/common/http";
-import { Observable } from "rxjs";
-import { Injectable } from "@angular/core";
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 @Injectable()
 export class InterceptorService implements HttpInterceptor {
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    console.log("Intercepted ", req.url);
-    if (req.url.search("/authenticate")!==-1 || req.url.search("/create")!==-1) {
+    console.log('Intercepted ', req.url);
+    if (req.url.search('/authenticate') !== -1 || req.url.search('/create') !== -1) {
       return next.handle(req);
-    }
-    else{
+    } else {
       const newReq = req.clone({
         headers: req.headers.append(
-          "Authorization",
-          "Bearer "+ this.getToken()
+          'Authorization',
+          'Bearer ' + this.getToken()
         )
       });
       console.log(newReq);
       return next.handle(newReq);
     }
   }
-  getToken(){
+  getToken() {
     return localStorage.getItem('token');
   }
 }
